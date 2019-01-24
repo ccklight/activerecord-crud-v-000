be6b5210b72e0e1421d73a92624477d47187ebcf
Cheryl K <cherylkelson@icloud.com>
2019-01-24 14:41:26 +0000

    Automatically backed up by Learn

--- db/migrate/20190124143608_create_movies.rb
+++ db/migrate/20190124143608_create_movies.rb
@@ -4,19 +4,13 @@ class CreateMovies < ActiveRecord::Migration[5.2]
     def change
     create_table :costume_stores do |t|
       t.string :title
-
       t.integer :release_date
       t.integer :costume_inventory
       t.string :director
-
       t.string :lead
-
       t.boolean :in_theaters
 
 
-
-      lead	(string)
-      in_theaters	(boolean)
 
 
         end
