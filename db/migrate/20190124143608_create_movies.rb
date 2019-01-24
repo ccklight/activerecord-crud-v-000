2195d716fc550b6bb18848d05044dce622dd94e8
Cheryl K <cherylkelson@icloud.com>
2019-01-24 14:42:05 +0000

    Automatically backed up by Learn

--- db/migrate/20190124143608_create_movies.rb
+++ db/migrate/20190124143608_create_movies.rb
@@ -1,20 +1,13 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
-  def change
+    def change
 
     def change
-    create_table :costume_stores do |t|
+    create_table : do |t|
       t.string :title
       t.integer :release_date
       t.integer :costume_inventory
       t.string :director
       t.string :lead
       t.boolean :in_theaters
-
-
-
-
         end
-    end
-
-  end
-end
+      end
