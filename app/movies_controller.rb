4e18444a13f2fbfd9d7d7380bb3db6e890378c6c
Cheryl K <cherylkelson@icloud.com>
2019-01-24 15:37:59 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -81,7 +81,6 @@ def can_update_using_update_method
   movie = Movie.find_by(title: "Wat, huh?")
   movie.update(title: "Wat, huh?")
   movie.save 
-  __
 end
 
 def can_update_multiple_items_at_once
