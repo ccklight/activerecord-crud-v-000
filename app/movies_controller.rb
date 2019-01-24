39b22813e2525b4c529726faa1c832bd406bb378
Cheryl K <cherylkelson@icloud.com>
2019-01-24 15:28:32 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -66,7 +66,6 @@ end
 def can_be_found_updated_and_saved
   Movie.create(title: "Awesome Flick")
    movie = Movie.create(title: "Awesome Flick")
-   #movie = Movie.find_by(title:"Awesome Flick")
    movie.update(title: "Even Awesomer Flick")
    movie.save
 end
