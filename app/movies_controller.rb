9a88edc731849627f2713b85ecd84ae9d8749ab7
Cheryl K <cherylkelson@icloud.com>
2019-01-24 15:32:41 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -59,9 +59,12 @@ def can_find_by_multiple_attributes
  Movie.find_by(title: "Title", release_date: 2000, director: "Me")
 end
 
+
 def can_find_using_where_clause_and_be_sorted
-  Movie.where("release_date" > 2002).order(release_date::desc)
+ Movie.where("release_date > 2002").order(release_date: :desc)
 end
+
+
 
 def can_be_found_updated_and_saved
   Movie.create(title: "Awesome Flick")
